cc667e8ad14c77301ec7721e0698bcf6e1de98db
David Robinson <dr6@sanger.ac.uk>
2017-12-11 17:01:18 +0000

    refactored 2017/11 to simplify and shorten code

--- 2017/11_hexed.py
+++ 2017/11_hexed.py
@@ -1,126 +1,63 @@
 #!/usr/bin/env python3
 
 import sys
-from collections import Counter
 
 def desc(n, pos, neg):
     if n>0:
-        print(n, pos)
-    elif n<0:
-        print(-n, neg)
+        return '%d %s'%(n, pos)
+    if n<0:
+        return '%d %s'%(-n, neg)
+    return ''
 
-def show_final_distance(split_data):
-    print("Part 1.")
-    moves = Counter(split_data)
-    opposites = (('nw', 'se'), ('ne', 'sw'), ('n', 's'))
-    for p,n in opposites:
-        moves[p] -= moves[n]
-        moves[n] = 0
-    nw, n, ne = [moves[i] for i in ('nw', 'n', 'ne')]
-    if nw>0 and ne>0:
-        m = min(nw,ne)
-        nw -= m
-        ne -= m
-        n += m
-    elif nw<0 and ne<0:
-        m = min(-nw, -ne)
-        nw += m
-        ne += m
-        n -= m
-    if n>0 and ne<0:
-        m = min(n, -ne)
-        n -= m
-        ne += m
-        nw += m
-    if n>0 and nw<0:
-        m = min(n, -nw)
-        n -= m
-        nw += m
-        ne += m
-    if n<0 and nw>0:
-        m = min(-n, nw)
-        n += m
-        ne -= m
-        nw -= m
-    if n<0 and ne>0:
-        m = min(-n, ne)
-        n += m
-        ne -= m
-        nw -= m
-    desc(nw, 'northwest', 'southeast')
-    desc(ne, 'northeast', 'southwest')
-    desc(n, 'north', 'south')
-    print("Total:", abs(nw)+abs(ne)+abs(n)) #781 -- too high
+def sign(n):
+    return (1 if n>1 else -1 if n<0 else 0)
 
-def show_max_distance(split_data):
-    n = 0
-    nw = 0
-    ne = 0
+def apply_step(pos, direction, delta, alt1, delta1, alt2, delta2):
+    if sign(pos[direction])==-delta:
+        pos[direction] += delta
+        return
+    if sign(pos[alt1])==-delta1 or sign(pos[alt2])==-delta2:
+        pos[alt1] += delta1
+        pos[alt2] += delta2
+        return
+    pos[direction] += delta
+
+def track_distance(split_data):
+    loc = {'n': 0, 'nw': 0, 'ne': 0}
     most_steps = 0
     for d in split_data:
-        steps = abs(n)+abs(nw)+abs(ne)
+        if d=='n':
+            apply_step(loc, 'n', 1, 'nw', 1, 'ne', 1)
+        elif d=='s':
+            apply_step(loc, 'n', -1, 'nw', -1, 'ne', -1)
+        elif d=='ne':
+            apply_step(loc, 'ne', 1, 'nw', -1, 'n', 1)
+        elif d=='nw':
+            apply_step(loc, 'nw', 1, 'ne', -1, 'n', 1)
+        elif d=='se':
+            apply_step(loc, 'nw', -1, 'ne', 1, 'n', -1)
+        elif d=='sw':
+            apply_step(loc, 'ne', -1, 'nw', 1, 'n', -1)
+        else:
+            raise ValueError(repr(d))
+        steps = sum(map(abs, loc.values()))
         if steps > most_steps:
             most_steps = steps
-        if d=='n':
-            if n>=0 and (nw < 0 or ne < 0):
-                nw += 1
-                ne += 1
-                continue
-            n += 1
-            continue
-        if d=='s':
-            if n<=0 and (ne > 0 or nw > 0):
-                ne -= 1
-                nw -= 1
-                continue
-            n -= 1
-            continue
-        if d=='ne':
-            if ne>=0 and (nw > 0 or n < 0):
-                nw -= 1
-                n += 1
-                continue
-            ne += 1
-            continue
-        if d=='se':
-            if nw<=0 and (ne < 0 or n > 0):
-                ne += 1
-                n -= 1
-                continue
-            nw -= 1
-            continue
-        if d=='nw':
-            if nw>=0 and (ne > 0 or n < 0):
-                ne -= 1
-                n += 1
-                continue
-            nw += 1
-            continue
-        if d=='sw':
-            if ne<=0 and (nw < 0 or n > 0):
-                nw += 1
-                n -= 1
-                continue
-            ne -= 1
-            continue
-        raise ValueError(repr(d))
-    steps = abs(n)+abs(nw)+abs(ne)
-    print("\nPart 2.")
-    print("Final position:")
-    desc(n, "north", "south")
-    desc(nw, "northwest", "southeast")
-    desc(ne, "northeast", "southwest")
-    if steps > most_steps:
-        most_steps = steps
-    print("Final distance:",steps)
+
+    positions = [desc(loc[i], pos, neg) for (i, pos, neg) in 
+                   (('n', "north", "south"),
+                    ('nw', "northwest", "southeast"),
+                    ('ne', "northeast", "southwest"))
+                if loc[i]]
+    print("Final position:", ', '.join(positions))
+    print("Final distance:", steps)
     print("Greatest distance:", most_steps)
             
     
 def main():
     data = sys.stdin.read()
     split_data = data.replace(',',' ').split()
-    show_final_distance(split_data)
-    show_max_distance(split_data)
+    track_distance(split_data)
 
 if __name__ == '__main__':
     main()
