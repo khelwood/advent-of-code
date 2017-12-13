142bd730594c73e933731ebae140d4811888c9f9
David Robinson <14000840+khelwood@users.noreply.github.com>
2017-12-13 18:18:52 +0000

    tidied up 2016/07

--- 2016/07_ip.py
+++ 2016/07_ip.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import pyperclip
+import sys
 
 def abba(txt):
     for i in range(len(txt)-3):
@@ -33,29 +33,22 @@ def separate(txt):
         good.append(txt[j:])
     return good, bad
 
+def supports_abba(txt):
+    good, bad = separate(txt)
+    return any(map(abba, good)) and not any(map(abba, bad))
+
 def supports_ssl(txt):
     good, bad = separate(txt)
     abas = set(find_abas(good))
     babs = [b+a+b for a,b,_ in abas]
     return any(bab in b for b in bad for bab in babs)
 
-def supports_abba(txt):
-    good, bad = separate(txt)
-    return any(map(abba, good)) and not any(map(abba, bad))
-
-def count(fn, lines):
-    return sum(map(fn, lines))
-
 def main():
-    print("Press enter to start")
-    input()
-    block = pyperclip.paste().strip()
-    lines = block.split('\n')
-    c = count(supports_abba, lines)
-    print("ABBA count:", c)
-    d = count(supports_ssl, lines)
-    print("SSL count:", d)
-        
+    lines = sys.stdin.read().strip().split('\n')
+    n1 = sum(map(supports_abba, lines))
+    print("ABBA count:", n1)
+    n2 = sum(map(supports_ssl, lines))
+    print("SSL count:", n2)
 
 if __name__ == '__main__':
     main()
