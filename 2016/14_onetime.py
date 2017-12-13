a1c6f3a4335637839b6695e6b332d6e3adbe82fb
David Robinson <14000840+khelwood@users.noreply.github.com>
2017-12-13 22:22:19 +0000

    tidied up 2016/14

--- 2016/14_onetime.py
+++ 2016/14_onetime.py
@@ -1,74 +1,60 @@
 #!/usr/bin/env python3
 
-from hashlib import md5
 import sys
 import re
+import hashlib
 
-PART = 2
-
-SALT = 'qzyelonm'
-ORD_A = ord('a')
-
-class Buzzes:
-    def __init__(self, init):
-        self.data = [init]*16
-    def tohex(self, ch):
-        if '0'<=ch<='9':
-            return int(ch)
-        return ord(ch) + 10 - ORD_A
-    def __getitem__(self, key):
-        return self.data[self.tohex(key)]
-    def __setitem__(self, key, value):
-        self.data[self.tohex(key)] = value
-    def __len__(self):
-        return len(self.data)
-    def __repr__(self):
-        return repr(self.data)
-
-def enumerate_hashes(salt, start=0, stop=None):
+def enumerate_hashes(salt, reps, start=0, stop=None):
     index = start
     while index!=stop:
-        h = get_hash(salt, index)
+        h = get_hash(salt, index, reps)
         yield (index, h)
         index += 1
 
-def get_hash(salt, index, cache=[]):
+def get_hash(salt, index, reps, caches={}):
+    cache = caches.get(reps)
+    if cache is None:
+        cache = []
+        caches[reps] = cache
     while len(cache) <= index:
-        cache.append(make_hash(salt+str(len(cache))))
+        cache.append(make_hash(salt+str(len(cache)), reps))
     return cache[index]
 
-if PART<2:
-    def make_hash(text):
-        return md5(text.encode('ascii')).hexdigest()
-else:
-    def make_hash(text):
-        for _ in range(2017):
-            text = md5(text.encode('ascii')).hexdigest()
-        return text
+def make_hash(text, reps):
+    for _ in range(reps):
+        text = hashlib.md5(text.encode('ascii')).hexdigest()
+    return text
     
-def find_keys(salt, ptn=re.compile(r'(.)\1\1')):
-    DURATION = 1000
-    buzzes = Buzzes(-2*DURATION)
-    for i,h in enumerate_hashes(salt):
+def find_keys(salt, reps, ptn=re.compile(r'(.)\1\1')):
+    for i,h in enumerate_hashes(salt, reps):
         m = ptn.search(h)
         if m:
             k = m.group(1)
             sought = k*5
-            if any(sought in v for _,v in enumerate_hashes(salt, i+1, i+1001)):
+            if any(sought in v
+                  for _,v in enumerate_hashes(salt, reps, i+1, i+1001)):
                 yield i
 
 
-def print_keys(salt):
+def print_keys(salt, reps):
     i = 0
-    for k in find_keys(salt):
-        print(k)
+    for k in find_keys(salt, reps):
+        print(k, end=' ', flush=True)
         i += 1
         if i>= 64:
-            break        
+            return k 
 
-def main(salt):
-    print_keys(salt)
+def main():
+    if len(sys.argv)<=1:
+        exit("Usage: %s <salt>"%sys.argv[0])
+    salt = sys.argv[1]
+    print("Part 1: ...")
+    k1 = print_keys(salt, 1)
+    print("\nPart 2: ...")
+    k2 = print_keys(salt, 2017)
+    print("\n\nPart 1 result:", k1)
+    print("Part 2 result:", k2)
+    
 
 if __name__ == '__main__':
-    salt = sys.argv[1] if len(sys.argv) > 1 else SALT
-    main(salt)
+    main()
