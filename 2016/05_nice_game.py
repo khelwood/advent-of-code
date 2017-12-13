f056f4f67dcd1d2b5a343ffc6da5a06da3075166
David Robinson <14000840+khelwood@users.noreply.github.com>
2017-12-13 16:59:52 +0000

    Fixed up 2016/05

--- 2016/05_nice_game.py
+++ 2016/05_nice_game.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
 
-from hashlib import md5
+import sys
+import hashlib
 
 def find_hashes(door):
     i = 0
-    ni = 100000
     while True:
         i += 1
-        if i>=ni:
-            print(f'    {i}   ', end='\r')
-            ni += 100000
         h = make_hash(door, i)
         if hash_ok(h):
             yield h
 
-def make_password(door, length=8):
+def first_password(door, length=8):
+    results = []
+    i = 0
+    for h in find_hashes(door):
+        results.append(h[5])
+        if len(results) >= length:
+            break
+    return ''.join(results)
+
+def second_password(door, length=8):
     results = ['.']*length
     found = 0
     for h in find_hashes(door):
-        hd = h.hexdigest()
-        i = ord(hd[5])-ord('0')
+        i = ord(h[5])-ord('0')
         if 0<=i<length and results[i]=='.':
-            results[i] = hd[6]
-            print('   %r   '%(''.join(results)))
+            results[i] = h[6]
+            print('  %s'%(''.join(results)), end='\r')
             found += 1
             if found >= length:
                 break
@@ -30,16 +35,22 @@ def make_password(door, length=8):
 
 def make_hash(door, i):
     s = door+str(i)
-    return md5(s.encode('ascii'))
+    return hashlib.md5(s.encode('ascii')).hexdigest()
 
 def hash_ok(h):
-    return h and h.hexdigest().startswith('00000')
+    return h.startswith('00000')
 
 def main():
-    door = input('Enter door id: ').strip()
-    print(f'Door id: {door}')
-    password = make_password(door)
-    print(f'\nPassword: {password}')
+    if len(sys.argv) <= 1:
+        exit("Usage: %s <doorid>"%sys.argv[0])
+    door = sys.argv[1]
+    print("Door id:", door)
+    print(" Working",end='\r')
+    pw = first_password(door)
+    print("First password:", pw)
+    print(" Working",end='\r')
+    pw = second_password(door)
+    print("Second password:", pw)
 
 if __name__ == '__main__':
     main()
