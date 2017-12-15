3a21470e1706b64b7f4610b186d5f3c3c25de92b
David Robinson <14000840+khelwood@users.noreply.github.com>
2017-12-15 13:40:13 +0000

    tidied up 2017/11

--- 2017/11_hexed.py
+++ 2017/11_hexed.py
@@ -2,62 +2,57 @@
 
 import sys
 
-def desc(n, pos, neg):
-    if n>0:
-        return '%d %s'%(n, pos)
-    if n<0:
-        return '%d %s'%(-n, neg)
-    return ''
+from collections import namedtuple
+
+Alternative = namedtuple('Alternative', 'dir sign')
+
+def read_alt(alt):
+    delta = -1 if alt.startswith('-') else 1
+    return Alternative(alt.lstrip('+-'), delta)
 
 def sign(n):
     return (1 if n>1 else -1 if n<0 else 0)
 
-def apply_step(pos, direction, delta, alt1, delta1, alt2, delta2):
-    if sign(pos[direction])==-delta:
-        pos[direction] += delta
-        return
-    if sign(pos[alt1])==-delta1 or sign(pos[alt2])==-delta2:
-        pos[alt1] += delta1
-        pos[alt2] += delta2
-        return
-    pos[direction] += delta
+class Direction:
+    def __init__(self, name, opposite, alts):
+        self.name = name
+        self.opposite = opposite
+        self.alts = [read_alt(alt) for alt in alts.split()]
+    def apply(self, loc, delta):
+        if (sign(loc[self.name]) != -delta
+                and any(sign(loc[alt.dir])==-delta*alt.sign
+                            for alt in self.alts)):
+            for alt in self.alts:
+                loc[alt.dir] += delta*alt.sign
+        else:
+            loc[self.name] += delta
+
+DIRECTIONS = (
+    Direction('n', 's', '+nw +ne'),
+    Direction('ne', 'sw', '-nw +n'),
+    Direction('nw', 'se', '-ne +n'),
+)
+
+DIR_DICT = { d.name: (d,1) for d in DIRECTIONS }
+DIR_DICT.update({ d.opposite: (d,-1) for d in DIRECTIONS })
 
 def track_distance(split_data):
-    loc = {'n': 0, 'nw': 0, 'ne': 0}
+    loc = { d.name: 0 for d in DIRECTIONS }
     most_steps = 0
     for d in split_data:
-        if d=='n':
-            apply_step(loc, 'n', 1, 'nw', 1, 'ne', 1)
-        elif d=='s':
-            apply_step(loc, 'n', -1, 'nw', -1, 'ne', -1)
-        elif d=='ne':
-            apply_step(loc, 'ne', 1, 'nw', -1, 'n', 1)
-        elif d=='nw':
-            apply_step(loc, 'nw', 1, 'ne', -1, 'n', 1)
-        elif d=='se':
-            apply_step(loc, 'nw', -1, 'ne', 1, 'n', -1)
-        elif d=='sw':
-            apply_step(loc, 'ne', -1, 'nw', 1, 'n', -1)
-        else:
-            raise ValueError(repr(d))
+        direction, delta = DIR_DICT[d]
+        direction.apply(loc, delta)
         steps = sum(map(abs, loc.values()))
         if steps > most_steps:
             most_steps = steps
 
-    positions = [desc(loc[i], pos, neg) for (i, pos, neg) in 
-                   (('n', "north", "south"),
-                    ('nw', "northwest", "southeast"),
-                    ('ne', "northeast", "southwest"))
-                if loc[i]]
-    print("Final position:", ', '.join(positions))
     print("Final distance:", steps)
     print("Greatest distance:", most_steps)
             
     
 def main():
-    data = sys.stdin.read()
-    split_data = data.replace(',',' ').split()
-    track_distance(split_data)
+    data = sys.stdin.read().replace(',',' ').split()
+    track_distance(data)
 
 if __name__ == '__main__':
     main()
