c77b4b5f0f3f92161e12847400d5450ec19c7bf4
David Robinson <14000840+khelwood@users.noreply.github.com>
2017-12-26 14:41:33 +0000

    simplified point.py

--- point.py
+++ point.py
@@ -5,41 +5,25 @@ a 2D point class called Point."""
 
 __all__ = ['Point']
 
-import operator
 import sys
 
 _PY = sys.version_info[0]
-
-if _PY < 3:
-    from itertools import izip as _zip
-else:
-    _zip = zip
-
-def coordfn(fn, swapped=False):
-    if swapped:
-        return lambda self, other: type(self)(fn(other[0], self[0]),
-                                              fn(other[1], self[1]))
-    return lambda self, other: type(self)(fn(self[0], other[0]),
-                                          fn(self[1], other[1]))
-
-def scalarfn(fn, swapped=False):
-    if swapped:
-        return lambda self, other: type(self)(fn(other, self[0]),
-                                              fn(other, self[1]))
-    return lambda self, other: type(self)(fn(self[0], other),
-                                          fn(self[1], other))
 
 class Point(tuple):
     '2D immutable point class'
     __slots__ = ()
     def __new__(cls, x, y):
         return tuple.__new__(cls, (x, y))
-    x = property(operator.itemgetter(0), doc='Alias for field 0')
-    y = property(operator.itemgetter(1), doc='Alias for field 1')
-    __add__ = coordfn(operator.add)
-    __radd__ = coordfn(operator.add, swapped=True)
-    __sub__ = coordfn(operator.sub)
-    __rsub__ = coordfn(operator.sub, swapped=True)
+    x = property(lambda self: self[0], doc='Alias for field 0')
+    y = property(lambda self: self[1], doc='Alias for field 1')
+    def __add__(self, other):
+        return type(self)(self[0]+other[0], self[1]+other[1])
+    def __radd__(self, other):
+        return type(self)(other[0]+self[0], other[1]+self[1])
+    def __sub__(self, other):
+        return type(self)(self[0]-other[0], self[1]-other[1])
+    def __rsub__(self, other):
+        return type(self)(other[0]-self[0], other[1]-self[1])
     def __str__(self):
         return '(%r,%r)'%self
     def __repr__(self):
@@ -48,13 +32,18 @@ class Point(tuple):
         return type(self)(-self[0], -self[1])
     def __pos__(self):
         return self
-    __mul__ = scalarfn(operator.mul)
-    __rmul__ = scalarfn(operator.mul, swapped=True)
-    __floordiv__ = scalarfn(operator.floordiv)
-    __truediv__ = scalarfn(operator.truediv)
-    __mod__ = scalarfn(operator.mod)
+    def __mul__(self, other):
+        return type(self)(self[0]*other, self[1]*other)
+    def __rmul__(self, other):
+        return type(self)(other*self[0], other*self[1])
+    def __floordiv__(self, other):
+        return type(self)(self[0]//other, self[1]//other)
+    def __truediv__(self, other):
+        return type(self)(self[0]/other, self[1]/other)
+    def __mod__(self, other):
+        return type(self)(self[0]%other, self[1]%other)
     def dot(self, other):
-        return sum(a*b for a,b in _zip(self, other))
+        return (self[0]*other[0] + self[1]*other[1])
     @classmethod
     def of(cls, p):
         return p if isinstance(p,cls) else cls(p[0], p[1])
@@ -72,7 +61,8 @@ class Point(tuple):
     def abs(cls, p):
         return cls(abs(p[0]), abs(p[1]))
     if _PY < 3:
-        __div__ = scalarfn(operator.div)
+        def __div__(self, other):
+            return type(self)(self[0]/other, self[1]/other)
 
 if __name__ == '__main__':
     p = Point(3,2)
