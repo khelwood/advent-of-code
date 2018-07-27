04ba693073c1095d07f0e0b198b58e5c827ebda3
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-07-27 15:33:42 +0100

    rewrote 2017/08

--- 2017/08_iheard.py
+++ 2017/08_iheard.py
@@ -1,68 +1,59 @@
 #!/usr/bin/env python3
 
 import sys
-import operator
 import re
+import operator
 
 from collections import defaultdict, namedtuple
 
-Command = namedtuple('Command', 'subject update amount value1 operator value2')
+Command = namedtuple('Command', 'target diff subject op value')
 
-OPS = {
-    '>': operator.gt,
-    '<': operator.lt,
-    '>=': operator.ge,
-    '<=': operator.le,
+class Program:
+    def __init__(self):
+        self.registers = defaultdict(int)
+        self.max_value = 0
+    def __setitem__(self, key, value):
+        self.registers[key] = value
+        if value > self.max_value:
+            self.max_value = value
+    def __getitem__(self, key):
+        return self.registers[key]
+
+OPERATORS = {
     '==': operator.eq,
     '!=': operator.ne,
+    '>': operator.gt,
+    '<': operator.lt,
+    '<=': operator.le,
+    '>=': operator.ge,
 }
+    
+def read_commands():
+    pattern = re.compile(r'@ (inc|dec) # if @ ([!=<>]+) #\s*$'
+                             .replace(' ',r'\s+')
+                             .replace('@', '([a-z]+)')
+                             .replace('#', '(-?[0-9]+)'))
+    for line in sys.stdin:
+        m = pattern.match(line)
+        if not m:
+            raise ValueError(repr(line))
+        target, incdec, diff, subject, op, value = m.groups()
+        diff, value = map(int, (diff, value))
+        if incdec=='dec':
+            diff = -diff
+        yield Command(target, diff, subject, OPERATORS[op], value)
 
-LEX_PTN = re.compile(r'^R R R if R\s*([<>=!]+)\s*R\s*$'
-    .replace('R',r'(-?\w+)').replace(' ',r'\s+'))
-
-def lex(line):
-    m = LEX_PTN.match(line)
-    if not m:
-        raise ValueError(line)
-    return Command(*map(m.group, range(1,7)))
-
-class Registers:
-    def __init__(self):
-        self._dict = defaultdict(int)
-    def __getitem__(self, name):
-        if name.startswith('-') or name.isdigit():
-            return int(name)
-        return self._dict[name]
-    def __setitem__(self, name, value):
-        self._dict[name] = value
-    def values(self):
-        return self._dict.values()
-
-def process(registers, line):
-    command = lex(line)
-    v1 = registers[command.value1]
-    v2 = registers[command.value2]
-    op = OPS[command.operator]
-    if op(v1, v2):
-        delta = registers[command.amount]
-        if command.update=='dec':
-            delta = -delta
-        elif command.update!='inc':
-            raise ValueError(line)
-        x = registers[command.subject] + delta
-        registers[command.subject] = x
-        return x
-
+def run(prog, commands):
+    for cmd in commands:
+        if cmd.op(prog[cmd.subject], cmd.value):
+            prog[cmd.target] += cmd.diff
+        
 def main():
-    lines = sys.stdin.read().strip().split('\n')
-    registers = Registers()
-    greatest = None
-    for line in lines:
-        v = process(registers, line)
-        if v is not None and (greatest is None or greatest < v):
-            greatest = v
-    print("Max value:", max(registers.values()))
-    print("Max ever value:", greatest)
+    commands = tuple(read_commands())
+    prog = Program()
+    run(prog, commands)
+    print("Max final value:", max(prog.registers.values()))
+    print("Max ever:", prog.max_value)
 
 if __name__ == '__main__':
     main()
