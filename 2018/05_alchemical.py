bc0c0042dea33c23233ec275ad01c1adf08b91b1
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-12-05 09:04:30 +0000

    2018/05

---
+++ 2018/05_alchemical.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+
+def pairs(x,y):
+    return (x!=y and x.upper()==y.upper())
+
+def react(seq):
+    i = 0
+    end = len(seq)-1
+    while i < end:
+        if pairs(seq[i], seq[i+1]):
+            del seq[i:i+2]
+            end -= 2
+            i -= 1
+        else:
+            i += 1
+
+def without(seq, letter):
+    rem = {letter.upper(), letter.lower()}
+    return [ch for ch in seq if ch not in rem]
+
+def main():
+    sequence = list(sys.stdin.read().strip())
+    react(sequence)
+    print("Reduced length:", len(sequence))
+    best_length = len(sequence)
+    for i in range(ord('a'), ord('z')+1):
+        letter = chr(i)
+        newseq = without(sequence, letter)
+        react(newseq)
+        if len(newseq) < best_length:
+            best_length = len(newseq)
+            best_letter = letter
+    print("Best letter to remove:", best_letter)
+    print("Best length:", best_length)
+
+if __name__ == '__main__':
+    main()
