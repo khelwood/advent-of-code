2788b7ef196eab2d1742729fdb5e9d7281085495
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-12-13 14:22:32 +0000

    2016/08 removed grid dependency

--- 2016/08_twofactor.py
+++ 2016/08_twofactor.py
@@ -3,63 +3,67 @@
 import sys
 import re
 
-from collections import namedtuple
-
-sys.path.append('..')
-
-from point import Point
-from grid import Grid
+from collections import namedtuple, defaultdict
 
 ON = '#'
 OFF = '.'
+WIDTH = 50
+HEIGHT = 6
 
-def compile(expr):
-    expr = expr.replace(' ',r'\s+').replace('#','([0-9]+)')
-    return re.compile('^'+expr+'$')
+Command = namedtuple('Command', 'pattern function')
+Command.commands = []
 
+def command(expr):
+    expr = '^' + expr.replace(' ',r'\s+').replace('#','([0-9]+)') + '$'
+    def command(func):
+        Command.commands.append(Command(re.compile(expr), func))
+        return func
+    return command
+
+@command('rect #x#')
 def fill_rect(grid, width, height):
     for i in range(width):
         for j in range(height):
             grid[i,j] = ON
 
-def rotate_row(grid, y, delta):
-    delta %= grid.width
+@command('rotate row y=# by #')
+def rotate_row(grid, y, delta, wid=WIDTH):
+    delta %= wid
     if delta==0:
         return
-    row = [grid[x,y] for x in range(grid.width)]
-    for x in range(grid.width):
+    row = [grid[x,y] for x in range(wid)]
+    for x in range(wid):
         grid[x,y] = row[x-delta]
 
-def rotate_column(grid, x, delta):
-    delta %= grid.height
+@command('rotate column x=# by #')
+def rotate_column(grid, x, delta, hei=HEIGHT):
+    delta %= hei
     if delta==0:
         return
-    column = [grid[x,y] for y in range(grid.height)]
-    for y in range(grid.height):
+    column = [grid[x,y] for y in range(hei)]
+    for y in range(hei):
         grid[x,y] = column[y-delta]
 
-Command = namedtuple('Command', 'pattern function')
-        
-COMMANDS = [ Command(compile(x), y) for (x,y) in
-                (('rect #x#', fill_rect),
-                 ('rotate row y=# by #', rotate_row),
-                 ('rotate column x=# by #', rotate_column))]
-
 def process(grid, line):
-    for cmd in COMMANDS:
+    for cmd in Command.commands:
         m = cmd.pattern.match(line)
         if m:
-            return cmd.function(grid, int(m.group(1)), int(m.group(2)))
+            args = [int(g) for g in m.groups()]
+            return cmd.function(grid, *args)
     raise ValueError(repr(line))
+
+def display(grid, wid=WIDTH, hei=HEIGHT):
+    for y in range(hei):
+        print(' '.join([grid[x,y] for x in range(wid)]))
 
 def main():
     lines = sys.stdin.read().strip().split('\n')
-    grid = Grid(50, 6, OFF)
+    grid = defaultdict(lambda: OFF)
     for line in lines:
         process(grid, line)
-    count_on = grid.count(ON)
+    count_on = sum(v==ON for v in grid.values())
     print("Count:", count_on)
-    print(grid)
+    display(grid)
 
 if __name__ == '__main__':
     main()
