435764f01f46dee5dfd08d4bd3dce6d86aa864b2
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-12-13 11:40:21 +0000

    2018/13 parts 1 and 2

--- 2018/13_mine.py
+++ 2018/13_mine.py
@@ -2,20 +2,16 @@
 
 import sys
 
-DIRECTIONS = {
-    '<' : (-1,0),
-    '>' : (+1,0),
-    'v' : (0,+1),
-    '^' : (0,-1),
-}
+DIRECTIONS = { '<' : (-1,0), '>' : (+1,0), 'v' : (0,+1), '^' : (0,-1), }
 
 class Mine:
     def __init__(self, lines):
-        self.carts = []
+        self.pos_carts = {}
         self.lines = tuple(self.extract_carts(line, y)
                            for y,line in enumerate(lines))
         self.width = max(map(len, lines))
         self.height = len(lines)
+        self.first_crash = None
     def extract_carts(self, line, y, directions=DIRECTIONS):
         for x in range(len(line)):
             direc = directions.get(line[x])
@@ -23,7 +19,7 @@ class Mine:
                 if isinstance(line, str):
                     line = list(line)
                 line[x] = '-' if direc[0] else '|'
-                self.carts.append(Cart(x,y, direc))
+                self.pos_carts[x,y] = Cart(x, y, direc)
         if isinstance(line, list):
             line = ''.join(line)
         return line
@@ -34,22 +30,28 @@ class Mine:
             if 0 <= x < len(line):
                 return line[x]
         return ' '
-    def __str__(self):
-        return '\n'.join(self.lines)
-    def tick_to_crash(self):
-        carts = sorted(self.carts)
-        positions = { cart.position: cart for cart in carts }
-        for cart in carts:
-            oldpos = cart.position
+    @property
+    def num_carts(self):
+        return len(self.pos_carts)
+    def tick(self):
+        pos_carts = self.pos_carts
+        for cart in sorted(pos_carts.values()):
+            if cart.crashed:
+                continue
+            del pos_carts[cart.position]
             cart.advance(self)
             newpos = cart.position
-            if newpos in positions:
-                return newpos # CRASH
-            positions[newpos] = positions[oldpos]
-            del positions[oldpos]
+            if newpos in pos_carts:
+                if self.first_crash is None:
+                    self.first_crash = newpos
+                other = pos_carts.pop(newpos)
+                other.crashed = True
+                cart.crashed = True
+            else:
+                pos_carts[newpos] = cart
     def display(self):
         ycarts = {}
-        for cart in self.carts:
+        for cart in self.pos_carts.values():
             if cart.y not in ycarts:
                 ycarts[cart.y] = []
             ycarts[cart.y].append(cart)
@@ -63,28 +65,26 @@ class Mine:
                 line[cart.x] = dir_rev[cart.direction]
             print(''.join(line))
 
-
 class Cart:
     def __init__(self, x, y, direction):
         self.x = x
         self.y = y
         self.direction = direction
         self.turns = 0
+        self.crashed = False
     def __lt__(self, other):
         return (self.y < other.y or self.y==other.y and self.x < other.x)
-    def __repr__(self):
-        return f'Cart({self.x}, {self.y}, {self.direction})'
     @property
     def position(self):
         return (self.x, self.y)
     def advance(self, mine):
-        v = mine[self.x, self.y]
+        track = mine[self.x, self.y]
         vx,vy = self.direction
-        if v=='/':
+        if track=='/':
             (vx,vy) = self.direction = (-vy, -vx)
-        elif v=='\\':
+        elif track=='\\':
             (vx,vy) = self.direction = (+vy, +vx)
-        elif v=='+':
+        elif track=='+':
             if self.turns==0:
                 self.turns = 1
                 (vx,vy) = self.direction = (vy,-vx)
@@ -98,12 +98,13 @@ class Cart:
 
 def main():
     mine = Mine(sys.stdin.read().splitlines())
-    while True:
+    while mine.first_crash is None:
         #mine.display()
-        crash = mine.tick_to_crash()
-        if crash:
-            break
-    print("First crash: %r,%r"%crash)
+        mine.tick()
+    print("First crash: %r,%r"%mine.first_crash)
+    while mine.num_carts > 1:
+        mine.tick()
+    print("Last cart position: %r,%r"%next(iter(mine.pos_carts)))
 
 if __name__ == '__main__':
     main()
