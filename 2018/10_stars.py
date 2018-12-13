d07dcb61c488a043687eee644808df7b22ae5d5e
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-12-13 12:27:04 +0000

    2018/10 - removed unnecessary dependency on grid

--- 2018/10_stars.py
+++ 2018/10_stars.py
@@ -3,10 +3,6 @@
 import sys
 import re
 
-sys.path.append('..')
-
-from grid import Grid
-
 class Star:
     def __init__(self, x, y, vx, vy):
         self.x = x
@@ -27,27 +23,15 @@ def read_star(line, ptn=INPUT_PTN):
         raise ValueError(repr(line))
     return Star(*map(int, m.groups()))
 
-def bounds(stars):
-    star = stars[0]
-    (x0,y0) = (x1,y1) = (star.x, star.y)
-    for star in stars:
-        x = star.x
-        y = star.y
-        x0 = min(x0, x)
-        y0 = min(y0, y)
-        x1 = max(x1, x)
-        y1 = max(y1, y)
-    return x0,y0,x1,y1
-
 def display(stars):
-    x0,y0,x1,y1 = bounds(stars)
-    x1 += 1
-    y1 += 1
-    grid = Grid(x1-x0, y1-y0, fill='.')
-    for star in stars:
-        grid[star.x-x0, star.y-y0] = '#'
-    for row in grid.rows():
-        print(''.join(row))
+    positions = {(star.x, star.y) for star in stars}
+    x0 = min(x for (x,y) in positions)
+    y0 = min(y for (x,y) in positions)
+    x1 = max(x for (x,y) in positions)
+    y1 = max(y for (x,y) in positions)
+    xran = range(x0,x1+1)
+    for y in range(y0,y1+1):
+        print(''.join(['#' if (x,y) in positions else '.' for x in xran]))
 
 def main():
     stars = [read_star(line) for line in sys.stdin.read().splitlines()]
