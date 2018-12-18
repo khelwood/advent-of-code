13122ac5c11553bbd79485b3009b8c1b0d25c40c
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-12-18 11:48:40 +0000

    2018/18 simplified and sped up
    Removed the mutable state class in favour of new frozenset state
    from the previous with a next_state function.

--- 2018/18_settlers.py
+++ 2018/18_settlers.py
@@ -9,105 +9,84 @@ YARD = '#'
 
 ADJACENT = ((-1,-1), (0,-1), (1,-1), (-1,0), (1,0), (-1,1), (0,1), (1,1))
 
-class Area:
-    def __init__(self, lines):
-        self.trees = set()
-        self.yards = set()
-        for y,line in enumerate(lines):
-            for x,value in enumerate(line.strip()):
-                if value==YARD:
-                    self.yards.add((x,y))
-                elif value==TREE:
-                    self.trees.add((x,y))
-        self.width = x+1
-        self.height = y+1
-    def __getitem__(self, pos):
-        if pos in self.trees:
-            return TREE
-        if pos in self.yards:
-            return YARD
-        return OPEN
-    def __setitem__(self, pos, value):
-        if value==TREE:
-            self.yards.discard(pos)
-            self.trees.add(pos)
-        elif value==YARD:
-            self.trees.discard(pos)
-            self.yards.add(pos)
-        else:
-            self.trees.discard(pos)
-            self.yards.discard(pos)
-    @property
-    def num_trees(self):
-        return len(self.trees)
-    @property
-    def num_yards(self):
-        return len(self.yards)
-    def count_adjacent_trees(self, pos, dirs=ADJACENT):
-        return sum((addp(pos, d) in self.trees) for d in dirs)
-    def count_adjacent_yards(self, pos, dirs=ADJACENT):
-        return sum((addp(pos, d) in self.yards) for d in dirs)
-    def advance(self):
-        removed_yards = []
-        new_yards = []
-        new_trees = []
-        trees = self.trees
-        yards = self.yards
-        for pos in itertools.product(range(self.width), range(self.height)):
-            if pos in trees:
-                if self.count_adjacent_yards(pos)>=3:
-                    new_yards.append(pos)
-            elif pos in yards:
-                if (self.count_adjacent_yards(pos)==0
-                        or self.count_adjacent_trees(pos)==0):
-                    removed_yards.append(pos)
-            else:
-                if self.count_adjacent_trees(pos)>=3:
-                    new_trees.append(pos)
-        yards.difference_update(removed_yards)
-        trees.difference_update(new_yards)
-        yards.update(new_yards)
-        trees.update(new_trees)
-    def freeze(self):
-        return (frozenset(self.yards), frozenset(self.trees))
-    def display(self):
-        for y in range(self.height):
-            print(''.join([self[x,y] for x in range(self.width)]))
-
 def addp(a,b):
     return (a[0]+b[0], a[1]+b[1])
 
+def count_adj(p, units, dirs=ADJACENT):
+    return sum((addp(p, d) in units) for d in dirs)
+
+def any_adj(p, units, dirs=ADJACENT):
+    return any((addp(p,d) in units) for d in dirs)
+
+def next_state(yards, trees):
+    new_yards = set()
+    removed_yards = set()
+    new_trees = set()
+    for pos in itertools.product(range(WIDTH), range(HEIGHT)):
+        if pos in trees:
+            if count_adj(pos, yards)>=3:
+                new_yards.add(pos)
+        elif pos in yards:
+            if not (any_adj(pos, trees) and any_adj(pos, yards)):
+                removed_yards.add(pos)
+        else:
+            if count_adj(pos, trees)>=3:
+                new_trees.add(pos)
+    yards = yards - removed_yards | new_yards
+    trees = trees - new_yards | new_trees
+    return yards, trees
+
+def read_state():
+    global WIDTH, HEIGHT
+    yards = set()
+    trees = set()
+    for y,line in enumerate(sys.stdin):
+        for x,value in enumerate(line.strip()):
+            if value==TREE:
+                trees.add((x,y))
+            elif value==YARD:
+                yards.add((x,y))
+    WIDTH = x+1
+    HEIGHT = y+1
+    return frozenset(yards), frozenset(trees)
+
+def display(yards, trees):
+    symbol = lambda p: TREE if p in trees else YARD if p in yards else OPEN
+    for y in range(HEIGHT):
+        print(''.join([symbol((x,y)) for x in range(WIDTH)]))
+
 def main():
-    area = Area(sys.stdin)
-    st = area.freeze()
+    first_target = 10
+    second_target = 10**9
+    st = read_state()
     states = [st]
     state_index = {st:0}
-    for i in range(10):
-        area.advance()
-        st = area.freeze()
+
+    for i in range(first_target):
+        st = next_state(*st)
         states.append(st)
         state_index[st] = i
-    print("Resource value after 10 minutes:", area.num_trees*area.num_yards)
-    target = 10**9
+    yards, trees = st
+    print("First result:", len(yards)*len(trees))
+
     repeat_start = None
     repeat_end = None
-    for i in range(10,target):
-        area.advance()
-        st = area.freeze()
+    for i in range(first_target, second_target):
+        st = next_state(*st)
         if st in state_index:
             repeat_start = state_index[st]
             repeat_end = i
             break
         state_index[st] = i
         states.append(st)
-    print("Pattern repeats between", repeat_start, "and", repeat_end)
+    print(f"(Pattern repeats between {repeat_start} and {repeat_end})")
     if repeat_end:
         period = repeat_end - repeat_start
-        index = repeat_start + (target - repeat_start) % period
+        index = repeat_start + (second_target - repeat_start) % period
         yards, trees = states[index]
-        print("Final result:", len(yards)*len(trees))
     else:
-        print("Final result:", area.num_trees*area.num_yards)
+        yards, trees = st
+    print("Final result:", len(yards)*len(trees))
 
 if __name__ == '__main__':
     main()
