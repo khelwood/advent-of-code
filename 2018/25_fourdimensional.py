3419f636ea3dc68f9cf90bc7b6e41c80ae41c87d
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-12-25 14:59:30 +0000

    2018/25 simpler version

--- 2018/25_fourdimensional.py
+++ 2018/25_fourdimensional.py
@@ -11,28 +11,15 @@ def manhattan(a, b):
                 + abs(a[2]-b[2]) + abs(a[3]-b[3]))
 
 def count_constellations(points):
-    point_constellations = {}
+    constellations = { p: {p} for p in points }
     for a,b in combinations(points,2):
-        if manhattan(a,b) <= 3:
-            ca = point_constellations.get(a)
-            cb = point_constellations.get(b)
-            if ca==cb:
-                if not ca:
-                    ca = {a,b}
-                    point_constellations[a] = point_constellations[b] = ca
-            elif ca and cb:
-                ca.update(cb)
-                for k in cb:
-                    point_constellations[k] = ca
-            elif ca:
-                ca.add(b)
-                point_constellations[b] = ca
-            elif cb:
-                cb.add(a)
-                point_constellations[a] = cb
-    constellations = set(map(frozenset, point_constellations.values()))
-    remaining = frozenset(points) - frozenset.union(*constellations)
-    return len(constellations) + len(remaining)
+        ca = constellations[a]
+        cb = constellations[b]
+        if ca is not cb and manhattan(a,b) <= 3:
+            ca.update(cb)
+            for p in cb:
+                constellations[p] = ca
+    return len(set(map(id, constellations.values())))
 
 def main():
     points = [read_point(ln) for ln in sys.stdin.read().strip().splitlines()]
