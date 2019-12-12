d47734094312c0f23a07502536b1ad8cacd83cc9
David Robinson <14000840+khelwood@users.noreply.github.com>
2019-12-12 12:05:04 +0000

    2019/12 parts 1 and 2
    Rewrote both parts, removing all the parts to do with 3D points,
    because the axes are completely indepdendent, so we can
    represent them all separately (which makes part 2 possible).

--- 2019/12_nbody.py
+++ 2019/12_nbody.py
@@ -3,86 +3,28 @@
 import sys
 import re
 from itertools import combinations, chain
-
-class Point(tuple):
-    __slots__ = ()
-    def __new__(cls, x, y, z):
-        return tuple.__new__(cls, (x,y,z))
-    x = property(lambda self: self[0], doc='Alias for field 0')
-    y = property(lambda self: self[1], doc='Alias for field 1')
-    def __add__(self, other):
-        return type(self)(self[0]+other[0], self[1]+other[1], self[2]+other[2])
-    def __radd__(self, other):
-        return type(self)(other[0]+self[0], other[1]+self[1], other[2]+self[2])
-    def __sub__(self, other):
-        return type(self)(self[0]-other[0], self[1]-other[1], self[2]-other[2])
-    def __rsub__(self, other):
-        return type(self)(other[0]-self[0], other[1]-self[1], other[2]-self[2])
-    def __str__(self):
-        return '(%r,%r,%r)'%self
-    def __repr__(self):
-        return type(self).__name__+str(self)
-    def __neg__(self):
-        return type(self)(-self[0], -self[1], -self[2])
-    def __pos__(self):
-        return self
-    def __mul__(self, other):
-        return type(self)(self[0]*other, self[1]*other, self[2]*other)
-    def __rmul__(self, other):
-        return type(self)(other*self[0], other*self[1], other*self[2])
-    def __floordiv__(self, other):
-        return type(self)(self[0]//other, self[1]//other, self[2]//other)
-    def __truediv__(self, other):
-        return type(self)(self[0]/other, self[1]/other, self[2]/other)
-    def __mod__(self, other):
-        return type(self)(self[0]%other, self[1]%other, self[2]%other)
-    def dot(self, other):
-        return (self[0]*other[0] + self[1]*other[1] + self[2]*other[2])
-    @classmethod
-    def of(cls, p):
-        return p if isinstance(p,cls) else cls(p[0], p[1], p[2])
-    @classmethod
-    def max(cls, *p):
-        if len(p)==1:
-            p = p[0]
-        return cls(max(i[0] for i in p), max(i[1] for i in p),
-                       max(i[2] for i in p))
-    @classmethod
-    def min(cls, *p):
-        if len(p)==1:
-            p = p[0]
-        return cls(min(i[0] for i in p), min(i[1] for i in p),
-                       min(i[2] for i in p))
-    @classmethod
-    def abs(cls, p):
-        return cls(abs(p[0]), abs(p[1]), abs(p[2]))
-
-class Body:
-    def __init__(self, pos):
-        self.pos = Point.of(pos)
-        self.vel = Point(0,0,0)
-    def advance(self):
-        self.pos += self.vel
-    def potential_energy(self):
-        return sum(Point.abs(self.pos))
-    def kinetic_energy(self):
-        return sum(Point.abs(self.vel))
-    def energy(self):
-        return self.potential_energy() * self.kinetic_energy()
-    def __repr__(self):
-        return 'pos=%r, vel=%r'%(self.pos, self.vel)
+from functools import reduce
 
 def sign(n):
     return -1 if n<0 else 0 if n==0 else 1
 
+class BodyAxis:
+    def __init__(self, pos):
+        self.pos = pos
+        self.vel = 0
+    def advance(self):
+        self.pos += vel
+    def state(self):
+        return (self.pos, self.vel)
+
 def apply_gravity(bodies):
     for alpha, beta in combinations(bodies, 2):
         diff = alpha.pos - beta.pos
-        acc = tuple(map(sign, diff))
-        alpha.vel -= acc
-        beta.vel += acc
+        diff = sign(diff)
+        beta.vel += diff
+        alpha.vel -= diff
 
-def parse_bodies(lines):
+def parse_positions(lines):
     pattern = re.compile('<x=#, y=#, z=#>'.replace(' ', r'\s*')
                              .replace('#', '(-?\d+)'))
     for line in lines:
@@ -91,33 +33,96 @@ def parse_bodies(lines):
             m = re.match(pattern, line)
             if not m:
                 raise ValueError("Unparsable line: %r"%line)
-            pos = Point.of(tuple(map(int, m.groups())))
-            yield Body(pos)
+            yield tuple(map(int, m.groups()))
 
-def state(bodies):
-    return tuple(chain.from_iterable((body.pos, body.vel) for body in bodies))
+def axis_state(bodies, index):
+    return tuple(chain.from_iterable((body.pos[index], body.vel[index])
+                                         for body in bodies))
 
 def time_step(bodies):
     apply_gravity(bodies)
     for body in bodies:
-        body.advance()
+        body.pos += body.vel
+
+def energy(axis_bodies):
+    total_energy = 0
+    for body in zip(*axis_bodies):
+        ke = sum(abs(ba.pos) for ba in body)
+        pe = sum(abs(ba.vel) for ba in body)
+        total_energy += ke*pe
+    return total_energy
+
+def find_repeat_slow(bodies):
+    states = {}
+    time = 0
+    while True:
+        new_state = tuple(chain.from_iterable(body.state() for body in bodies))
+        if new_state in states:
+            previous = states[new_state]
+            return (previous, time-previous)
+        states[new_state] = time
+        time_step(bodies)
+        time += 1
+
+def find_repeat_fast(bodies):
+    initial_pos = tuple(body.pos for body in bodies)
+    time = 0
+    while True:
+        time_step(bodies)
+        time += 1
+        if (all(body.vel==0 for body in bodies) and
+               all(body.pos==pos for (body, pos) in zip(bodies, initial_pos))):
+            return (0, time)
+
+def hcf(a,b):
+    a = abs(a)
+    b = abs(b)
+    if b < a:
+        a,b = b,a
+    while a:
+        a,b = b%a, a
+    return b
+
+def lcm(a, b):
+    return a*b // hcf(a,b)
+
+def find_combined_period(repeats):
+    if all(start==0 for (start,period) in repeats):
+        return reduce(lcm, (period for (start,period) in repeats))
+
+    # This takes a long time (minutes, not years)
+    longest_period = 0
+    for index, (start, period) in enumerate(repeats):
+        if period > longest_period:
+            longest_period = period
+            longest_index = index
+    start,period = repeats[longest_index]
+    repeats = [r for (i,r) in enumerate(repeats) if i!=longest_index]
+    time = start
+    while True:
+        time += period
+        if all((time-axis_start)%axis_period==0
+                   for (axis_start, axis_period) in repeats):
+            return time
 
 def main():
-    initial_data = list(sys.stdin)
-    bodies = list(parse_bodies(initial_data))
+    positions = tuple(parse_positions(sys.stdin))
+    axis_bodies = [[BodyAxis(pos[i]) for pos in positions] for i in range(3)]
     steps = 1000
     for _ in range(steps):
-        time_step(bodies)
-    print("Energy:", sum(body.energy() for body in bodies))
-    bodies = list(parse_bodies(initial_data))
-    states = {}
-    for n in range(100_000):
-        new_state = state(bodies)
-        if new_state in states:
-            print("State %s recurred at %s"%(states[new_state], n))
-        states[new_state] = n
-        time_step(bodies)
-    print("(no repeat - need something clever)")
+        for bodies in axis_bodies:
+            time_step(bodies)
+    print("Energy:", energy(axis_bodies))
+    print("(finding periods for each axis)")
+    axis_bodies = [[BodyAxis(pos[i]) for pos in positions] for i in range(3)]
+
+    # We're going to assume that all moons return to their initial state,
+    # because it makes stuff work faster.
+    find_repeat = find_repeat_fast
+
+    repeats = [find_repeat(bodies) for bodies in axis_bodies]
+    print("(finding combined period)")
+    print("Time:", find_combined_period(repeats))
 
 if __name__ == '__main__':
     main()
