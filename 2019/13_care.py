f1934f8a011a3ca875827bdc2c9009e7541055cc
David Robinson <14000840+khelwood@users.noreply.github.com>
2019-12-13 09:45:05 +0000

    2019/13 added option to render game

--- 2019/13_care.py
+++ 2019/13_care.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import time
 from program import Program, parse_program_input
 
 EMPTY_N = 0
@@ -14,7 +15,7 @@ LEFT_J = -1
 RIGHT_J = 1
 
 class Game:
-    def __init__(self, prog_input):
+    def __init__(self, prog_input, frame_rate=None):
         self.score = 0
         self.screen = {}
         self.joystick = NEUTRAL_J
@@ -23,6 +24,8 @@ class Game:
         self.prog.output_func = self.prog_output
         self.ball_x = 0
         self.paddle_x = 0
+        self.frame_rate = frame_rate
+        self.next_render = 0 if frame_rate else None
     def prog_output(self, value):
         if len(self.prog.output_values)%3==0:
             po = self.prog.output_values
@@ -36,6 +39,8 @@ class Game:
         if value==PADDLE_N:
             self.paddle_x = x
     def joy_input(self):
+        if self.frame_rate is not None and self.next_render <= time.time():
+            self.render()
         bx = self.ball_x
         px = self.paddle_x
         return RIGHT_J if bx > px else LEFT_J if bx < px else NEUTRAL_J
@@ -46,6 +51,31 @@ class Game:
         self.prog.execute()
     def count_blocks(self):
         return sum(v==BLOCK_N for v in self.screen.values())
+    def render(self):
+        print(chr(27) + "[2J")
+        screen = self.screen
+        x0 = min(x for (x,y) in screen if x>=0)
+        y0 = min(y for (x,y) in screen if x>=0)
+        x1 = max(x for (x,y) in screen) + 1
+        y1 = max(y for (x,y) in screen) + 1
+        data = '\n'.join([''.join(render_tile(screen.get((x,y), EMPTY_N))
+                              for x in range(x0,x1))
+                              for y in range(y0,y1)])
+        print(data)
+        self.next_render = time.time() + self.frame_rate
+
+def render_tile(n):
+    if n==EMPTY_N:
+        return ' '
+    if n==BLOCK_N:
+        return '%'
+    if n==BALL_N:
+        return '*'
+    if n==PADDLE_N:
+        return '-'
+    if n==WALL_N:
+        return '\u2588'
+    return str(n)
 
 
 def main():
