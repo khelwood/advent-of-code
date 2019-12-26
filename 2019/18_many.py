42414268080a9adc68fab73986dac517771483c8
David Robinson <14000840+khelwood@users.noreply.github.com>
2019-12-26 21:08:45 +0000

    2019/18 part 2 (took much less time than part 1)

--- 2019/18_many.py
+++ 2019/18_many.py
@@ -3,7 +3,7 @@
 import sys
 
 DOOR_BIT = 64
-DOOR_S = '#'
+WALL_S = '#'
 START_S = '@'
 
 def neighbours(pos):
@@ -20,11 +20,11 @@ def key_int_remove(keys, key):
     return (keys & ~(1<<key))
 
 class Maze:
-    def __init__(self, grid, keys, doors, start, width, height):
+    def __init__(self, grid, keys, doors, starts, width, height):
         self.grid = grid
         self.keys = keys
         self.doors = doors
-        self.start = start
+        self.starts = starts
         self.width = width
         self.height = height
     def is_door(self, pos):
@@ -39,9 +39,10 @@ def parse_maze(lines):
     grid = {}
     keydict = {}
     doordict = {}
+    starts = []
     for y, line in enumerate(lines):
         for x, value in enumerate(line):
-            if value != DOOR_S:
+            if value != WALL_S:
                 pos = (x,y)
                 if value.isupper():
                     key_index = ord(value) - ord('A')
@@ -52,7 +53,7 @@ def parse_maze(lines):
                     keydict[key_index] = pos
                     value = key_index
                 elif value=='@':
-                    start = pos
+                    starts.append(pos)
                     value = 0
                 else:
                     value = 0
@@ -63,16 +64,17 @@ def parse_maze(lines):
     keys = [keydict[k] for k in key_values]
     doors = [doordict[k] for k in key_values]
 
-    maze = Maze(grid, keys, doors, start, width, height)
-    maze.start_route = make_routing(grid, start)
+    maze = Maze(grid, keys, doors, starts, width, height)
+    maze.start_route = make_routing(grid, starts)
     assert set(maze.start_route)==set(maze.grid)
+    maze.keyzones = find_keyzones(maze)
     maze.distances = distances(maze)
     maze.required_keys = find_required_keys(maze)
     return maze
 
-def make_routing(grid, start):
-    routes = {start:0}
-    new = [start]
+def make_routing(grid, starts):
+    routes = {start:0 for start in starts}
+    new = list(starts)
     steps = 0
     while new:
         steps += 1
@@ -93,9 +95,9 @@ def distances(maze):
         distances[keya][keya] = 0
         if None not in dista:
             continue
-        routes = make_routing(maze.grid, posa)
+        routes = make_routing(maze.grid, (posa,))
         for keyb, posb in enumerate(maze.keys):
-            if dista[keyb] is None:
+            if dista[keyb] is None and posb in routes:
                 dista[keyb] = routes[posb]
                 distances[keyb][keya] = routes[posb]
     return distances
@@ -139,6 +141,58 @@ def total_distance(maze, at_key, keys_needed, _cache={}):
     _cache[cache_key] = best_distance
     return best_distance
 
+def complex_distance(maze, positions, at_key, keys_needed, _cache={}):
+    if keys_needed==0:
+        return 0
+    zone = maze.keyzones[at_key]
+    positions = positions[:zone] + (at_key,) + positions[zone+1:]
+    cache_key = (positions, keys_needed)
+    distance = _cache.get(cache_key)
+    if distance is not None:
+        return distance
+    best_distance = (1<<30)
+    for key,pos in enumerate(maze.keys):
+        if ((1<<key)&keys_needed)==0:
+            continue
+        if (maze.required_keys[key] & keys_needed):
+            continue
+        zone = maze.keyzones[key]
+        zonekey = positions[zone]
+        if zonekey is not None:
+            distance = maze.distances[zonekey][key]
+        else:
+            distance = maze.start_route[pos]
+        distance += complex_distance(maze, positions, key,
+                                    key_int_remove(keys_needed, key))
+        best_distance = min(best_distance, distance)
+    _cache[cache_key] = best_distance
+    return best_distance
+
+def index_which(seq, predicate):
+    return next(i for (i,item) in enumerate(seq) if predicate(item))
+
+def partition_maze(lines):
+    y = index_which(lines, lambda line: (START_S in line))
+    line = lines[y]
+    x = line.index(START_S)
+    lines[y] = line[:x-1] + '###' + line[x+2:]
+    line = lines[y-1]
+    lines[y-1] = line[:x-1] + '@#@' + line[x+2:]
+    line = lines[y+1]
+    lines[y+1] = line[:x-1] + '@#@' + line[x+2:]
+
+def find_keyzones(maze):
+    nk = len(maze.keys)
+    if len(maze.starts)==1:
+        return [0] * nk
+    zones = [None]*nk
+    for zone, start in enumerate(maze.starts):
+        routes = make_routing(maze.grid, (start,))
+        for key, pos in enumerate(maze.keys):
+            if pos in routes:
+                zones[key] = zone
+    return zones
+
 def solve(maze):
     nk = len(maze.keys)
     keys_needed = (1 << nk)-1
@@ -151,12 +205,30 @@ def solve(maze):
         best_distance = min(distance, best_distance)
     return best_distance
 
+def solve_complex(maze):
+    nk = len(maze.keys)
+    keys_needed = (1<<nk)-1
+    best_distance = (1<<30)
+    positions = (None,) * len(maze.starts)
+    for key, pos in enumerate(maze.keys):
+        if maze.required_keys[key]:
+            continue
+        distance = (maze.start_route[pos]
+            + complex_distance(maze, positions, key,
+                        key_int_remove(keys_needed, key)))
+        best_distance = min(distance, best_distance)
+    return best_distance
 
 def main():
     lines = sys.stdin.read().strip().splitlines()
     maze = parse_maze(lines)
     distance = solve(maze)
-    print("Best distance:", distance)
+    print("Best distance (1):", distance)
+    # Part 2
+    partition_maze(lines)
+    maze = parse_maze(lines)
+    distance = solve_complex(maze)
+    print("Best distance (2):", distance)
 
 if __name__ == '__main__':
     main()
