481842a38c0dadd6a85fc2ac45e428f4671e77b1
David Robinson <14000840+khelwood@users.noreply.github.com>
2019-12-30 18:12:58 +0000

    2019/20 part 2
    Almost the same logic, just with a 3D route grid and some extra
    rules about portals.

--- 2019/20_donut.py
+++ 2019/20_donut.py
@@ -15,8 +15,10 @@ def neighbours(pos):
     yield (x+1, y)
 
 class Maze:
-    def __init__(self, grid, portals, start, end):
+    def __init__(self, grid, width, height, portals, start, end):
         self.grid = grid
+        self.width = width
+        self.height = height
         self.portals = portals
         self.start = start
         self.end = end
@@ -29,16 +31,38 @@ class Maze:
         if isinstance(val, int):
             pp0, pp1 = self.portals[val]
             yield (pp1 if pp0==pos else pp0)
+    def adjacent_3D(self, pos):
+        grid = self.grid
+        px,py,pz = pos
+        xy = (px,py)
+        for nbr in neighbours(xy):
+            if nbr in grid:
+                yield nbr + (pz,)
+        val = grid[xy]
+        if isinstance(val, int):
+            inner = (xy in self.inner_portals)
+            if pz==0 and not inner:
+                return
+            pp0, pp1 = self.portals[val]
+            pp = (pp1 if pp0==xy else pp0)
+            if inner:
+                yield pp + (pz+1,)
+            else:
+                yield pp + (pz-1,)
 
 def parse_maze(data):
     pos_letter = {}
     grid = {}
+    width = None
     for y,line in enumerate(data.splitlines()):
+        if width is None:
+            width = len(line)
         for x,value in enumerate(line):
             if value.isalpha():
                 pos_letter[x,y] = value
             elif value==SPACE_S:
                 grid[x,y] = value
+    height = y+1
 
     code_pos = defaultdict(list)
 
@@ -72,7 +96,7 @@ def parse_maze(data):
             grid[pos] = pi
         portals.append(portal_loc)
 
-    return Maze(grid, portals, start, end)
+    return Maze(grid, width, height, portals, start, end)
 
 def make_route(maze, start):
     grid = maze.grid
@@ -90,17 +114,31 @@ def make_route(maze, start):
                     new.append(nbr)
     return route
 
+def distance_3D(maze, start, end):
+    assert len(start)==3
+    grid = maze.grid
+    route = {start: 0}
+    new = [start]
+    steps = 0
+    while new:
+        old = new
+        new = []
+        steps += 1
+        for p in old:
+            for nbr in maze.adjacent_3D(p):
+                if nbr not in route:
+                    route[nbr] = steps
+                    if nbr==end:
+                        return steps
+                    new.append(nbr)
+
 def portal_locations(grid, pos):
     return [nbr for nbr in neighbours(pos) if grid.get(nbr)==SPACE_S]
 
 def draw_maze(maze):
     grid = maze.grid
-    y0 = min(y for (_,y) in grid)
-    y1 = max(y for (_,y) in grid)
-    x0 = min(x for (x,_) in grid)
-    x1 = max(x for (x,_) in grid)
-    for y in range(y0, y1+1):
-        for x in range(x0, x1+1):
+    for y in range(maze.height):
+        for x in range(maze.width):
             value = grid.get((x,y), ' ')
             if isinstance(value, int) and value >= 10:
                 value = chr(ord('A') + value - 10)
@@ -110,13 +148,28 @@ def draw_maze(maze):
     print("End:", maze.end)
     print("Portals:", maze.portals)
 
+def find_inner_portals(maze):
+    x0 = 3
+    x1 = maze.width-3
+    y0 = 3
+    y1 = maze.height-3
+    inner = set()
+    for pi,poss in enumerate(maze.portals):
+        for (x,y) in poss:
+            if x0 < x < x1 and y0 < y < y1:
+                inner.add((x,y))
+    return inner
+
 def main():
     data = sys.stdin.read().rstrip()
     maze = parse_maze(data)
     draw_maze(maze)
-    maze.route = make_route(maze, maze.start)
-    distance = maze.route[maze.end]
-    print("Distance to end:", distance)
+    route = make_route(maze, maze.start)
+    distance = route[maze.end]
+    print("Distance flat:", distance)
+    maze.inner_portals = find_inner_portals(maze)
+    distance = distance_3D(maze, maze.start+(0,), maze.end+(0,))
+    print("Distance 3D:", distance)
 
 if __name__ == '__main__':
     main()
