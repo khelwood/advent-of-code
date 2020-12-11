6cd11bdf291e58102311fbde90553f88bf1e2770
David Robinson <14000840+khelwood@users.noreply.github.com>
2020-12-11 19:16:45 +0000

    2020/11 shorter

--- 2020/11_seating.py
+++ 2020/11_seating.py
@@ -6,117 +6,46 @@ FLOOR = '.'
 EMPTY = 'L'
 OCCUPIED = '#'
 
+DIRECTIONS = ((-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0))
+
 def adjacent_simple(layout, pos):
     grid = layout.grid
     x,y = pos
-    for xx in (x-1, x, x+1):
-        for yy in (y-1, y, y+1):
-            p = (xx,yy)
-            if p in grid and p!=pos:
-                yield p
+    for dx,dy in DIRECTIONS:
+        p = (x + dx, y + dy)
+        if p in grid:
+            yield p
 
 def adjacent_long(layout, pos):
     grid = layout.grid
     width = layout.width
     height = layout.height
     x,y = pos
-    xx = x-1
-    yy = y-1
-    while xx >= 0 and yy >= 0:
-        p = (xx,yy)
-        if p in grid:
-            yield p
-            break
-        xx -= 1
-        yy -= 1
-    xx = x-1
-    while xx >= 0:
-        p = (xx,y)
-        if p in grid:
-            yield p
-            break
-        xx -= 1
-    xx = x-1
-    yy = y+1
-    while xx >= 0 and yy < height:
-        p = (xx,yy)
-        if p in grid:
-            yield p
-            break
-        xx -= 1
-        yy += 1
-    yy = y+1
-    while yy < height:
-        p = (x,yy)
-        if p in grid:
-            yield p
-            break
-        yy += 1
-    xx = x+1
-    yy = y+1
-    while xx < width and yy < height:
-        p = (xx,yy)
-        if p in grid:
-            yield p
-            break
-        xx += 1
-        yy += 1
-    xx = x+1
-    while xx < width:
-        p = (xx,y)
-        if p in grid:
-            yield p
-            break
-        xx += 1
-    xx = x+1
-    yy = y-1
-    while xx < width and yy >= 0:
-        p = (xx,yy)
-        if p in grid:
-            yield p
-            break
-        xx += 1
-        yy -= 1
-    yy = y-1
-    while yy >= 0:
-        p = (x, yy)
-        if p in grid:
-            yield p
-            break
-        yy -= 1
-
+    for dx,dy in DIRECTIONS:
+        xx = x+dx
+        yy = y+dy
+        while 0 <= xx < width and 0 <= yy < height:
+            p = (xx,yy)
+            if p in grid:
+                yield p
+                break
+            xx += dx
+            yy += dy
 
 class Layout:
     def __init__(self, width, height):
-        self.grid = {}
         self.width = width
         self.height = height
 
     def find_adjacent_seats(self, function):
         self.adj = {pos:list(function(self, pos)) for pos in self.grid}
 
-    def __getitem__(self, pos):
-        return self.grid.get(pos, FLOOR)
-
-    def __setitem__(self, pos, value):
-        x,y = pos
-        if 0 <= x < self.width and 0 <= y < self.height:
-            if value==FLOOR:
-                self.grid.pop(pos, None)
-            else:
-                self.grid[pos] = value
-        else:
-            raise ValueError(pos)
-
     def count(self, value):
         return sum(v==value for v in self.grid.values())
 
     def advance_grid(self):
-        self.grid = self.next_grid()
-
-    def next_grid(self):
         func = self.next_value
-        return {pos:func(pos) for pos in self.grid}
+        self.grid = {pos:func(pos) for pos in self.grid}
 
     def next_value(self, pos):
         grid = self.grid
@@ -140,11 +69,12 @@ def parse_layout(string):
     width = len(lines[0])
     height = len(lines)
     layout = Layout(width, height)
-    grid = layout.grid
+    grid = {}
     for y,line in enumerate(lines):
         for x,value in enumerate(line):
             if value != FLOOR:
                 grid[x,y] = value
+    layout.grid = grid
     return layout
 
 
