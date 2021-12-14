a8346298d0beeb25df59b84063bb6ef15ad65f59
David Robinson <14000840+khelwood@users.noreply.github.com>
2021-12-14 13:12:36 +0000

    docstrings for 2021/14 because it required actual thinking

--- 2021/d14_polymer.py
+++ 2021/d14_polymer.py
@@ -3,14 +3,21 @@
 import sys
 from collections import Counter
 
-def read_data():
+def read_data() -> (str, dict):
     lines = sys.stdin.read().strip().splitlines()
     template = lines[0]
     rules = {a.strip():b.strip() for (a,_,b) in
             (line.partition('->') for line in lines if '->' in line)}
     return template, rules
 
-def grow(pairs, rules):
+def grow(pairs: dict, rules: dict) -> dict:
+    """
+    Advance a generation by replacing or keeping a pair's count.
+    E.g. if rule is "AB" -> "C"
+    then "AB":5 is converted to "AC":5, "CB":5.
+    If there is no matching rule, "AB":5 is incorporated in the
+    next generation.
+    """
     new_pairs = Counter()
     for pair, count in pairs.items():
         value = rules.get(pair)
@@ -22,18 +29,26 @@ def grow(pairs, rules):
             new_pairs[pair] += count
     return new_pairs
 
-
-def freqdiff(pairs, first, last):
-    counter = Counter()
+def freqdiff(pairs: dict, first, last) -> int:
+    """
+    The difference between the most and least frequent character.
+    Because we are counting characters each pair, each
+    pair will be counted twice, except for the character
+    at the start and the end. So account for them, and
+    divide the overall answer by 2.
+    """
+    counter = Counter((first, last))
     for pair, count in pairs.items():
         a,b = pair
         counter[a] += count
         counter[b] += count
-    counter[first] += 1
-    counter[last] += 1
     return (max(counter.values()) - min(counter.values()))//2
 
-def compile_pairs(template):
+def compile_pairs(template: str) -> dict:
+    """
+    Convert the starting template into a collection of pairs
+    of adjacent characters. Track the count of each pair.
+    """
     pairs = Counter()
     for i in range(len(template)-1):
         pair = template[i] + template[i+1]
