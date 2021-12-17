f9270434e542b3c0640d4e0529ff422cb59ee7ef
David Robinson <14000840+khelwood@users.noreply.github.com>
2021-12-17 11:21:04 +0000

    2021/17 precompute the valid vxs and vys and use the cartesian product

--- 2021/d17_shot.py
+++ 2021/d17_shot.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+import itertools
 from dataclasses import dataclass
 
 @dataclass(frozen=True)
@@ -24,7 +25,10 @@ def read_target():
     m = re.match(pattern, text)
     return Target(*map(int, m.groups()))
 
-def track_path(target, vel):
+def track_path(target, vel) -> int:
+    """Follows the path of the shot.
+    If it hits, returns the height at its highest point.
+    If it misses, returns -1."""
     pos = (0,0)
     height = 0
     while hope(pos, vel, target):
@@ -59,6 +63,20 @@ def find_vys(ty0, ty1):
             vy -= 1
     return possible_vy
 
+def find_vxs(tx0, tx1):
+    possible_vx = set()
+    for vx0 in range(tx1+1):
+        x = 0
+        vx = vx0
+        while vx < tx1 and (vx > 0 or vx >= tx0):
+            if tx0 <= x <= tx1:
+                possible_vx.add(vx0)
+                break
+            x += vx
+            if vx > 0:
+                vx -= 1
+    return possible_vx
+
 def update_vel(v):
     vx,vy = v
     if vx > 0:
@@ -71,16 +89,15 @@ def main():
     target = read_target()
     best_height = 0
     count = 0
+    vxs = find_vxs(target.x0, target.x1)
     vys = find_vys(target.y0, target.y1)
-    for x in range(target.x1+1):
-        for y in vys:
-            h = track_path(target, (x,y))
-            if h >= 0:
-                count += 1
-                best_height = max(h, best_height)
+    for v in itertools.product(vxs, vys):
+        h = track_path(target, v)
+        if h >= 0:
+            count += 1
+            best_height = max(h, best_height)
     print("best height:", best_height)
     print("count:", count)
-
 
 if __name__ == '__main__':
     main()
