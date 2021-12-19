7f2ed58bc971f9164176cb4fe835aa7d2df98e2f
David Robinson <14000840+khelwood@users.noreply.github.com>
2021-12-19 19:24:00 +0000

    2021/19 tidied up

--- 2021/d19_scanners.py
+++ 2021/d19_scanners.py
@@ -1,140 +1,102 @@
 #!/usr/bin/env python3
 
 import sys
-import re
-import itertools
+
+from itertools import combinations
+from typing import NamedTuple
+from collections import defaultdict
 from ast import literal_eval as leval
 
-def addp(a,b):
-    return (a[0]+b[0], a[1]+b[1], a[2]+b[2])
+class Point(NamedTuple):
+    x:int
+    y:int
+    z:int
 
-def subp(a,b):
-    return (a[0]-b[0], a[1]-b[1], a[2]-b[2])
-
-def dot(a,b):
-    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
-
-def manhattan(a,b):
-    return abs(a[0]-b[0]) + abs(a[1]-b[1]) + abs(a[2]-b[2])
+    def __add__(self, other):
+        return Point(self[0]+other[0], self[1]+other[1], self[2]+other[2])
+    def __sub__(self, other):
+        return Point(self[0]-other[0], self[1]-other[1], self[2]-other[2])
+    def __neg__(self):
+        return Point(-self[0], -self[1], -self[2])
+    def __mul__(self, other):
+        return self[0]*other[0] + self[1]*other[1] + self[2]*other[2]
+    def abs(self):
+        return Point(abs(self[0]), abs(self[1]), abs(self[2]))
+    def d1(self):
+        return sum(self.abs())
 
 def setup_rotations():
-    x = (1,0,0)
-    y = (0,1,0)
-    z = (0,0,1)
-    _x = (-1,0,0)
-    _y = (0,-1,0)
-    _z = (0,0,-1)
-    rots = [(x,y,z), (_x,z,y), (z,_y, x), (y,x,_z),
-            (_x,_y, z), (x,_y,_z), (_x,y,_z),
-            (_x,_z,_y)]
+    x = Point(1,0,0)
+    y = Point(0,1,0)
+    z = Point(0,0,1)
+    rots = [(x,y,z), (-x,z,y), (z,-y, x), (y,x,-z),
+            (-x,-y, z), (x,-y,-z), (-x,y,-z), (-x,-z,-y)]
     return rots + [(b,c,a) for (a,b,c) in rots] + [(c,a,b) for (a,b,c) in rots]
 
-class Rotation:
-    __slots__ = ('matrix',)
-    def __init__(self, matrix):
-        self.matrix = matrix
-    def __repr__(self):
-        return f'Rotation{self.matrix}'
-    def __str__(self):
-        return 'Rotation:\n' + '\n'.join(map(str, self.matrix))
+class Rotation(tuple):
     def __call__(self, v):
-        m = self.matrix
-        return tuple(dot(a,v) for a in self.matrix)
+        return Point(*(a*v for a in self))
 
 ROTATIONS = tuple(map(Rotation, setup_rotations()))
 
-def sort3(a,b,c):
-    if a <= b:
-        if a <= c:
-            if b <= c:
-                return (a,b,c)
-            return (a,c,b)
-        return (c,a,b)
-    if b <= c:
-        if c <= a:
-            return (b, c, a)
-        return (b, a, c)
-    return (c,b,a)
-
 class Scanner:
-    def __init__(self, index):
-        self.index = index
+    def __init__(self):
         self.beacons = []
         self.fixed = False
 
-    def calculate_differences(self):
-        diffs = {}
-        for a in self.beacons:
-            adiffs = set()
-            for b in self.beacons:
-                if a is not b:
-                    dx,dy,dz = subp(a,b)
-                    dx = abs(dx)
-                    dy = abs(dy)
-                    dz = abs(dz)
-                    adiffs.add(sort3(dx,dy,dz))
-            diffs[a] = adiffs
-        self.diffs = diffs
-
-    def compare_differences(self, other):
-        for p1,d1 in self.diffs.items():
-            for p2,d2 in other.diffs.items():
-                if len(d1&d2) >= 11:
-                    return (p1,p2)
-        return None,None
-
     def fix(self, disp, rot):
-        self.position = disp or (0,0,0)
+        self.position = disp or Point(0,0,0)
         if disp is not None:
-            self.beacons = [addp(disp, rot(p)) for p in self.beacons]
+            self.beacons = [disp + rot(p) for p in self.beacons]
+            self.symmetric_diffs = calculate_differences(self.beacons, True)
         self.beacon_set = set(self.beacons)
-        self.fixed_diffs = calculate_fixed_diffs(self.beacons)
-        if disp is not None:
-            self.calculate_differences()
+        self.true_diffs = calculate_differences(self.beacons, False)
         self.fixed = True
 
-    def __repr__(self):
-        return f'scanner {self.index}'
+def calculate_differences(beacons, symmetric=False):
+    diffs = defaultdict(set)
+    for a,b in combinations(beacons, 2):
+        d = b-a
+        if symmetric:
+            d = Point(*sorted(d.abs()))
+            diffs[a].add(d)
+            diffs[b].add(d)
+        else:
+            diffs[a].add(d)
+            diffs[b].add(-d)
+    diffs.default_factory = None
+    return diffs
 
-
-def calculate_fixed_diffs(beacons):
-    fixed_diffs = {}
-    for a in beacons:
-        adiffs = set()
-        for b in beacons:
-            if a is b:
-                continue
-            adiffs.add(subp(b,a))
-        fixed_diffs[a] = adiffs
-    return fixed_diffs
+def compare_differences(adiffs, bdiffs):
+    for p1,d1 in adiffs.items():
+        for p2,d2 in bdiffs.items():
+            if len(d1&d2) >= 11:
+                return (p1,p2)
+    return (None,None)
 
 def read_scanners():
     scanners = []
-    ptn = re.compile(r'--- scanner (\d+) ---')
     for line in sys.stdin.read().strip().splitlines():
         if ',' in line:
-            sc.beacons.append(leval(line))
-        else:
-            m = re.match(ptn, line)
-            if m:
-                sc = Scanner(int(m.group(1)))
-                scanners.append(sc)
+            sc.beacons.append(Point(*leval(line)))
+        elif 'scanner' in line:
+            sc = Scanner()
+            scanners.append(sc)
     return scanners
 
 def find_rotation(a, b, p1, p2):
-    adiffs = a.fixed_diffs[p1]
-    for r in ROTATIONS:
-        bp = r(p2)
-        bpoints = [r(p) for p in b.beacons]
-        bdiffs = {subp(p,bp) for p in bpoints if p!=bp}
+    adiffs = a.true_diffs[p1]
+    for rot in ROTATIONS:
+        bp = rot(p2)
+        bdiffs = {rot(p)-bp for p in b.beacons}
         if len(bdiffs&adiffs) >= 11:
-            return r
+            return rot
     return None
 
 def main():
     scanners = read_scanners()
     for sc in scanners:
-        sc.calculate_differences()
+        sc.symmetric_diffs = calculate_differences(sc.beacons, True)
     sc = scanners[0]
     sc.fix(None, None)
 
@@ -143,22 +105,24 @@ def main():
     for a in fixed:
         new_fixed = []
         for b in unfixed:
-            p1,p2 = a.compare_differences(b)
+            p1,p2 = compare_differences(a.symmetric_diffs, b.symmetric_diffs)
             if p1 is None:
                 continue
             rot = find_rotation(a, b, p1, p2)
             if rot is None:
                 continue
             rotp = rot(p2)
-            disp = subp(p1, rotp)
+            disp = p1 - rotp
             b.fix(disp, rot)
             fixed.append(b)
         unfixed = [sc for sc in unfixed if not sc.fixed]
+    assert not unfixed
+
     all_beacons = set.union(*(sc.beacon_set for sc in scanners))
     print("Number of beacons:", len(all_beacons))
 
-    max_manhattan = max(manhattan(a.position, b.position)
-         for (a,b) in itertools.combinations(scanners, 2))
+    max_manhattan = max((a.position-b.position).d1()
+                        for (a,b) in combinations(scanners, 2))
     print("Max distance:", max_manhattan)
 
 
