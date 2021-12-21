0ae762776f625dbaa36207aa2d6abc6e6530e9d9
David Robinson <14000840+khelwood@users.noreply.github.com>
2021-12-21 13:41:30 +0000

    2021/21 both parts, first working version

--- 2021/d21_dirac.py
+++ 2021/d21_dirac.py
@@ -12,6 +12,10 @@ Die rolls 1 then 2 then 3 ... 100, then 1 then 2 ...
 import sys
 from itertools import cycle
 from dataclasses import dataclass
+from collections import Counter
+from typing import NamedTuple
+
+ROLLS = {3:1, 4:3, 5:6, 6:7, 7:6, 8:3, 9:1}
 
 @dataclass
 class Player:
@@ -31,16 +35,66 @@ def play_to_end(players, die):
             if pl.score >= 1000:
                 return die_rolls
 
+class PlayState(NamedTuple):
+    space_1: int
+    space_2: int
+    score_1: int=0
+    score_2: int=0
+    turn: int=0
+
+def play_dirac(starts, all_rolls=ROLLS.items()):
+    state = PlayState(*starts)
+    state_counter = Counter(state)
+    win_counter = [0,0]
+    states = {state}
+    state_counter[state] += 1
+    while states:
+        state = states.pop()
+        c = state_counter[state]
+        if c==0:
+            continue
+        state_counter[state] = 0
+        for roll,freq in all_rolls:
+            st = next_state(state, roll)
+            if st.score_1 >= 21:
+                win_counter[0] += c*freq
+            elif st.score_2 >= 21:
+                win_counter[1] += c*freq
+            else:
+                states.add(st)
+                state_counter[st] += c*freq
+        print(" states: ", len(states), end='\r')
+    return win_counter
+
+def next_state(last, roll):
+    turn = last.turn
+    if turn==0:
+        pos = (last.space_1 + roll-1)%10 + 1
+        score = last.score_1 + pos
+        return PlayState(pos, last.space_2, score, last.score_2, 1)
+    pos = (last.space_2 + roll - 1) % 10 + 1
+    score = last.score_2 + pos
+    return PlayState(last.space_1, pos, last.score_1, score, 0)
+
+
 def main():
     if len(sys.argv) != 3:
         exit(f"Usage: {sys.argv[0]} <start1> <start2>")
     starts = (int(sys.argv[1]), int(sys.argv[2]))
+
     players = tuple(map(Player, starts))
     die_rolls = play_to_end(players, cycle(range(1,101)))
     print("Die rolls:", die_rolls)
     losing_score = min(pl.score for pl in players)
     print("Losing score:", losing_score)
     print("Result:", die_rolls * losing_score)
+
+    w1,w2 = play_dirac(starts)
+    if w1 > w2:
+        print("Player 1 wins in", w1, "universes.")
+    else:
+        print("player 2 wins in", w2, "universes.")
+
 
 
 if __name__ == '__main__':
