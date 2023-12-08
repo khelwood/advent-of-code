0424890154b3ba9f05cbf036de245380964f9de2
David Robinson <14000840+khelwood@users.noreply.github.com>
2023-12-08 21:15:40 +0000

    2023/07 simplified

--- 2023/d07_camel.py
+++ 2023/d07_camel.py
@@ -4,12 +4,12 @@ import sys
 import re
 from enum import Enum
 from collections import Counter
-from dataclasses import dataclass
 
 LINE_PTN = re.compile(r'(\w+)\s+(\d+)')
 
 VALUES = {str(c):c for c in range(2,10)}
 VALUES.update(zip('TJQKA', range(10,15)))
+WILDCARD = 1
 
 class HandType(Enum):
     HIGH_CARD = 1
@@ -25,44 +25,39 @@ class HandType(Enum):
 
 
 class Hand:
-    def __init__(self, cards):
+    def __init__(self, cards, bid):
         self.cards = tuple(cards)
         self.type = handtype(self.cards)
+        self.bid = bid
+
     def __lt__(self, other):
         if self.type < other.type:
             return True
         if self.type > other.type:
             return False
         return self.cards < other.cards
+
     def __len__(self):
         return len(self.cards)
+
     def __getitem__(self, i):
         return self.cards[i]
-    def __eq__(self, other):
-        return self.type==other.type and self.cards==other.cards
-    def make_wild(self):
-        self.cards = tuple(1 if c==11 else c for c in self)
-        self.type = wild_handtype(self.cards)
-
-
-@dataclass
-class HandBid:
-    hand: Hand
-    bid: int
-
-    def __eq__(self, other):
-        return self.hand==other.hand and self.bid==other.bid
-
-    def __lt__(self, other):
-        return self.hand < other.hand
 
     def make_wild(self):
-        self.hand.make_wild()
+        self.cards = tuple(WILDCARD if c==11 else c for c in self)
+        self.type = handtype(self.cards)
         
 
-def handtype(c):
-    if not isinstance(c, Counter):
-        c = Counter(c)
+def handtype(cards):
+    c = Counter(cards)
+    jcount = c[WILDCARD]
+    if jcount > 0 and len(c) > 1:
+        cs = c.most_common(2)
+        mv,_ = cs[0]
+        if mv==WILDCARD:
+            mv,_ = cs[1]
+        c[mv] += jcount
+        c[WILDCARD] = 0
     cs = c.most_common(2)
     mc = cs[0][1]
     if mc==5:
@@ -76,41 +71,21 @@ def handtype(c):
         return HandType.TWO_PAIR if nc==2 else HandType.PAIR
     return HandType.HIGH_CARD
 
-def wild_handtype(hand):
-    c = Counter(hand)
-    jv = 1
-    jcount = c[jv]
-    if jcount==0 or len(c)==1:
-        return handtype(c)
-    cs = c.most_common()
-    mv,_ = cs[0]
-    if mv==jv:
-        mv,_ = cs[1]
-    c[mv] += jcount
-    c[jv] = 0
-    return handtype(c)
-
-def parse_hand(string):
-    return Hand(VALUES[ch] for ch in string)
-
-
-def parse_line(line):
+def parse_hand(line):
     m = LINE_PTN.match(line)
-    hand = parse_hand(m.group(1))
-    bid = int(m.group(2))
-    return HandBid(hand,bid)
+    return Hand((VALUES[ch] for ch in m.group(1)), int(m.group(2)))
 
-def total_score(hbs):
-    return sum(i*hb.bid for (i, hb) in enumerate(hbs, 1))
+def total_score(hands):
+    return sum(i*hand.bid for (i, hand) in enumerate(hands, 1))
 
 def main():
-    hbs = list(map(parse_line, sys.stdin.read().splitlines()))
-    hbs.sort()
-    print("Part 1:", total_score(hbs))
-    for hb in hbs:
-        hb.make_wild()
-    hbs.sort()
-    print("Part 2:", total_score(hbs))
+    hands = list(map(parse_hand, sys.stdin.read().splitlines()))
+    hands.sort()
+    print("Part 1:", total_score(hands))
+    for hand in hands:
+        hand.make_wild()
+    hands.sort()
+    print("Part 2:", total_score(hands))
 
 if __name__ == '__main__':
     main()
