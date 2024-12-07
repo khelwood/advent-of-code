0e247ce7ef18b87a3b73fe763d6a7e3af1fa8479
David Robinson <14000840+khelwood@users.noreply.github.com>
2024-12-07 14:10:56 +0000

    2024/07 faster

--- 2024/d07_bridge.py
+++ 2024/d07_bridge.py
@@ -7,22 +7,27 @@ from itertools import product
 
 def parse_line(line):
     a,_,b = line.partition(':')
-    return int(a), [int(n) for n in b.split()]
+    return int(a), tuple(int(n) for n in b.split())
 
 def read_input():
     return list(map(parse_line, filter(bool, map(str.strip, sys.stdin))))
 
 def solvable(target, numbers, ops):
-    num_ops = len(numbers)-1
-    for comb in product(ops, repeat=num_ops):
-        n = numbers[0]
-        for i in range(num_ops):
-            n = comb[i](n, numbers[i+1])
-            if n > target:
-                break
-        if n==target:
+    if len(numbers)==1:
+        return target in numbers
+    a = numbers[0]
+    b = numbers[1]
+    c = numbers[2:]
+    for op in ops:
+        n = op(a,b)
+        if not c:
+            if n==target:
+                return True
+            continue
+        if n <= target and solvable(target, (n,)+c, ops):
             return True
     return False
+
 
 def concat(a,b):
     p = 10 if b==0 else (10**(int(math.log(b, 10))+1))
