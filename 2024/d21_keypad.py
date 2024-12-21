802bb9c6319fe693b05ad336b5b2053c7a5bbb5c
David Robinson <14000840+khelwood@users.noreply.github.com>
2024-12-21 21:01:50 +0000

    2024/21 simplified

--- 2024/d21_keypad.py
+++ 2024/d21_keypad.py
@@ -13,26 +13,14 @@ class Point(tuple):
         return Point(a-b for (a,b) in zip(self,p))
     def __neg__(self):
         return Point(-a for a in self)
-    def unit(self):
-        return Point(map(unit, self))
-    def d1(self):
-        x,y = self
-        return abs(x) + abs(y)
-    def origin(self):
-        return not any(self)
 
-def unit(n):
+def sign(n):
     return 1 if n > 0 else (-1) if n < 0 else 0
-
-def manhattan(p, q):
-    return abs(p[0]-q[0]) + abs(p[1]-q[1])
 
 ORIGIN = Point.at(0,0)
 RIGHT,DOWN,LEFT,UP = map(Point, zip((1,0,-1,0), (0,1,0,-1)))
-DIRS = (RIGHT,DOWN,LEFT,UP)
-DIR_CH = {RIGHT:'>', LEFT:'<', UP:'^', DOWN:'v', 'A':'A'}
 
-NUMPAD = {'A':ORIGIN}|dict(zip('0123456789',(LEFT,
+NUMPAD = dict(zip('A0123456789',(ORIGIN, Point.at(-1,0),
     Point.at(-2,-1), Point.at(-1,-1), Point.at(0,-1),
     Point.at(-2,-2), Point.at(-1,-2), Point.at(0,-2),
     Point.at(-2,-3), Point.at(-1,-3), Point.at(0,-3),
@@ -40,109 +28,50 @@ NUMPAD = {'A':ORIGIN}|dict(zip('0123456789',(LEFT,
 
 DIRPAD = {'A':ORIGIN, UP:LEFT, LEFT:Point.at(-2,1), DOWN:Point.at(-1,1), RIGHT:DOWN}
 
+BIG = 1_000_000_000_000_000
+
+class SingularDict:
+    def __init__(self, n):
+        self.n = n
+    def __getitem__(self, _):
+        return self.n
+
 def read_codes():
     return list(filter(bool, map(str.strip, sys.stdin)))
 
-
-def paths(p, cache={ORIGIN:((),)}):
-    v = cache.get(p)
-    if v is not None:
-        return v
-    u = p.unit()
+def paths(p):
+    u = Point(map(sign, p))
     ax,ay = map(abs, p)
     if ax==0:
-        v = (ay*(u,),)
-    elif ay==0:
-        v = (ax*(u,),)
-    else:
-        found = []
-        ux = Point.at(u[0], 0)
-        uy = Point.at(0, u[1])
-        for path in paths(p-ux):
-            found.append((ux,) + path)
-        for path in paths(p-uy):
-            found.append((uy,) + path)
-        v = tuple(found)
-    cache[p] = v
-    return v
+        return (ay*(u,),)
+    if ay==0:
+        return (ax*(u,),)
+    vx = ax*(Point.at(u[0], 0),)
+    vy = ay*(Point.at(0, u[1]),)
+    return (vx+vy, vy+vx)
 
-def path_str(path):
-    return ''.join(DIR_CH[ch] for ch in path)
+def calculate_cost(pos, path, valid_positions, master_costs):
+    cost = 0
+    master_pos = 'A'
+    for u in path:
+        pos += u
+        if pos not in valid_positions:
+            return BIG
+        cost += master_costs[master_pos, u]
+        master_pos = u
+    cost += master_costs[master_pos, 'A']
+    return cost
 
-BIG = 1_000_000_000_000_000
-
-def cost_dirpad2():
+def cost_pad(pad, master_costs):
     costs = {}
-    valid_positions = set(DIRPAD.values())
-    for va,vb in product(DIRPAD, repeat=2):
-        a = DIRPAD[va]
-        b = DIRPAD[vb]
+    valid_positions = set(pad.values())
+    for va,vb in product(pad, repeat=2):
+        a = pad[va]
+        b = pad[vb]
         best_cost = BIG
         for path in paths(b-a):
-            cost = 0
-            pos = a
-            valid = True
-            for u in path:
-                new = pos + u
-                if new not in valid_positions:
-                    valid = False
-                    break
-                cost += 1
-                pos = new
-            cost += 1 # press A
-            if valid and cost < best_cost:
-                best_cost = cost
-        costs[va,vb] = best_cost
-    return costs
-
-def cost_dirpad1(master_costs):
-    costs = {}
-    valid_positions = set(DIRPAD.values())
-    for va,vb in product(DIRPAD, repeat=2):
-        a = DIRPAD[va]
-        b = DIRPAD[vb]
-        best_cost = BIG
-        for path in paths(b-a):
-            cost = 0
-            pos = a
-            valid = True
-            master_pos = 'A'
-            for u in path:
-                new = pos + u
-                if new not in valid_positions:
-                    valid = False
-                    break
-                cost += master_costs[master_pos, u]
-                master_pos = u
-                pos = new
-            cost += master_costs[master_pos, 'A'] # ???back to A (origin)
-            if valid and cost < best_cost:
-                best_cost = cost
-        costs[va,vb] = best_cost
-    return costs
-
-def cost_numpad(master_costs):
-    costs = {}
-    valid_positions = set(NUMPAD.values())
-    for va,vb in product(NUMPAD, repeat=2):
-        a = NUMPAD[va]
-        b = NUMPAD[vb]
-        best_cost = BIG
-        for path in paths(b-a):
-            cost = 0
-            pos = a
-            valid = True
-            master_pos = 'A'
-            for u in path:
-                new = pos + u
-                if new not in valid_positions:
-                    valid = False
-                    break
-                cost += master_costs[master_pos,u]
-                master_pos = u
-                pos = new
-            cost += master_costs[master_pos, 'A'] # back to A (origin)
-            if valid and cost < best_cost:
+            cost = calculate_cost(a, path, valid_positions, master_costs)
+            if cost < best_cost:
                 best_cost = cost
         costs[va,vb] = best_cost
     return costs
@@ -162,23 +91,18 @@ def total_complexity(costs, codes):
         total += int(code.rstrip('A')) * cost
     return total
 
-def part1(codes):
-    pad2_costs = cost_dirpad2()
-    pad1_costs = cost_dirpad1(pad2_costs)
-    numpad_costs = cost_numpad(pad1_costs)
-    print(total_complexity(numpad_costs, codes))
-
-def part2(codes):
-    costs = cost_dirpad2()
-    for _ in range(24):
-        costs = cost_dirpad1(costs)
-    costs = cost_numpad(costs)
-    print(total_complexity(costs, codes))
+def pad_costs(codes, num_middle):
+    costs = cost_pad(DIRPAD, SingularDict(1))
+    for _ in range(num_middle):
+        costs = cost_pad(DIRPAD, costs)
+    return cost_pad(NUMPAD, costs)
 
 def main():
     codes = read_codes()
-    part1(codes)
-    part2(codes)
-
+    costs = pad_costs(codes, 1)
+    print(total_complexity(costs, codes))
+    costs = pad_costs(codes, 24)
+    print(total_complexity(costs, codes))
+    
 if __name__=='__main__':
     main()
