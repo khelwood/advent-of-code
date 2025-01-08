416ad1c03dcefe2f54f27eb08bf651de60208188
David Robinson <14000840+khelwood@users.noreply.github.com>
2025-01-08 15:07:48 +0000

    2024/24 advances towards part 2

--- 2024/d24_wires.py
+++ 2024/d24_wires.py
@@ -5,7 +5,8 @@ import re
 import operator
 
 from collections import defaultdict
-from typing import NamedTuple
+from dataclasses import dataclass
+from itertools import chain
 
 OPS = {
     'OR': operator.or_,
@@ -13,20 +14,69 @@ OPS = {
     'XOR': operator.xor,
 }
 
-class Value(NamedTuple):
+OP_ORDER = {op:i for i,op in enumerate((operator.or_, operator.xor, operator.and_))}
+
+OP_STR = {operator.or_:'|', operator.and_:'&', operator.xor:'^'}
+
+def link_op(op):
+    return lambda self,other: Link(self, other, op, '')
+
+class Linkable:
+    __or__ = link_op(operator.or_)
+    __xor__ = link_op(operator.xor)
+    __and__ = link_op(operator.and_)
+
+@dataclass
+class Value(Linkable):
     value: int
     vo: str
 
-class Link(NamedTuple):
+    def canonicalise(self):
+        pass
+
+    def __lt__(self, other):
+        if isinstance(other, Link):
+            return True
+        return self.vo < other.vo
+
+    def __call__(self):
+        return self.value
+
+    def __repr__(self):
+        return self.vo
+
+@dataclass
+class Link(Linkable):
     v1: str
     v2: str
     op: callable
     vo: str
 
-    def __call__(self, vars):
-        v1 = vars[self.v1]
-        v2 = vars[self.v2]
-        return self.op(v1,v2)
+    def __call__(self):
+        return self.op(self.v1(), self.v2())
+
+    def canonicalise(self):
+        self.v1.canonicalise()
+        self.v2.canonicalise()
+        if self.v2 < self.v1:
+            self.v1,self.v2 = self.v2,self.v1
+
+    def __lt__(self, other):
+        if isinstance(other, Value):
+            return False
+        if self.v1 < other.v1:
+            return True
+        if other.v1 < self.v1:
+            return False
+        n = OP_ORDER[self.op] - OP_ORDER[other.op]
+        if n < 0:
+            return True
+        if n > 0:
+            return False
+        return self.v2 < other.v2
+
+    def __repr__(self):
+        return f'{self.v1}{OP_STR[self.op]}{self.v2}'
 
 
 def read_input():
@@ -44,52 +94,34 @@ def read_input():
             links.append(Link(v1,v2,op,vo))
     return values, links
 
-def dependencies(values, links):
-    known = {v.vo for v in values}
-    precedes = defaultdict(set)
-    follows = defaultdict(set)
-    for link in links:
-        f = follows[link.vo]
-        if link.v1 not in known:
-            f.add(link.v1)
-            precedes[link.v1].add(link.vo)
-        if link.v2 not in known:
-            f.add(link.v2)
-            precedes[link.v2].add(link.vo)
-    return precedes, follows
+def nest_links(values, links):
+    d = {lk.vo:lk for lk in chain(values, links)}
+    for lk in links:
+        lk.v1 = d[lk.v1]
+        lk.v2 = d[lk.v2]
 
-def topsort(precedes, follows, links):
-    output = []
-    new = [x for x in precedes if not follows[x]]
-    done = set()
-    while new:
-        done.update(new)
-        cur = new
-        new = set()
-        for nv in cur:
-            output.append(nv)
-            for f in precedes[nv]:
-                nvf = follows[f]
-                nvf.remove(nv)
-                if not nvf:
-                    new.add(f)
-            del precedes[nv]
-    for link in links:
-        if link.vo not in done:
-            output.append(link.vo)
-    return output
+def create_adder(values):
+    x = {int(v.vo[1:]):v for v in values if v.vo.startswith('x')}
+    y = {int(v.vo[1:]):v for v in values if v.vo.startswith('y')}
+    top = max(x)
+    z = {}
+    z[0] = x[0]^y[0]
+    carry = x[0]&y[0]
+    for i in range(1, top + 1):
+        xi = x[i]
+        yi = y[i]
+        x_xor_y = xi^yi
+        z[i] = x_xor_y^carry
+        carry = (xi&yi) | (x_xor_y & carry)
+    z[top+1] = carry
+    for k,lk in z.items():
+        lk.vo = f'z{k:02d}'
+    for lk in z.values():
+        lk.canonicalise()
+    return {lk.vo:lk for lk in z.values()}
 
-def resolve_values(values, links, link_order):
-    d = {v.vo:v.value for v in values}
-    link_dict = {lk.vo : lk for lk in links}
-    for vo in link_order:
-        lk = link_dict.get(vo)
-        if lk:
-            d[vo] = lk(d)
-    return d
-
-def zvalue(d):
-    zd = {k:v for k,v in d.items() if k.startswith('z')}
+def zvalue(links):
+    zd = {lk.vo:lk() for lk in links if lk.vo.startswith('z')}
     z = 0
     for zv in sorted(zd, reverse=True):
         z = (z << 1) | zd[zv]
@@ -97,11 +129,16 @@ def zvalue(d):
 
 def main():
     values, links = read_input()
-    precedes, follows = dependencies(values, links)
+    nest_links(values, links)
+    for link in links:
+        link.canonicalise()
+    print(zvalue(links))
 
-    link_order = topsort(precedes, follows, links)
-    value_dict = resolve_values(values, links, link_order)
-    print(zvalue(value_dict))
+    links = {lk.vo:lk for lk in links}
+    adder = create_adder(values)
+    for z in adder.values():
+        if repr(links[z.vo]) != repr(z):
+            print(z.vo, ':', z)
 
 if __name__ == '__main__':
     main()
